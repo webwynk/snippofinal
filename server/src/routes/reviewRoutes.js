eb24341d2f67576881a2c839017d760ddcf19fbd
webwynke <webwynk@gmail.com>
2026-03-17 23:23:15 +0530

    Fix runtime error: use requireAuth in reviewRoutes.js

--- server/src/routes/reviewRoutes.js
+++ server/src/routes/reviewRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { createReview, getStaffReviews } from "../controllers/reviewController.js";
-import { protect } from "../auth.js";
+import { requireAuth } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, createReview);
+router.post("/", requireAuth(["user"]), createReview);
 router.get("/staff/:staffId", getStaffReviews);
 
 export default router;
